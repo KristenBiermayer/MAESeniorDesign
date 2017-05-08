c2022780b6b937d0e7e74404e54769dadbe90981
Susan Su <susansu@ucla.edu>
2017-05-07 18:04:57 -0700

    Create Code_01.ino

---
+++ Skeleton/Code_01.ino
@@ -0,0 +1,91 @@
+// declare global variables
+
+// declare variables that set up ranges for robot's distance to the wall
+const float FAR_WALL_THRESHOLD = 16;
+const float NEAR_WALL_THRESHOLD = 0;
+
+// declare variables that set up ranges for robot's distance to floor 
+const float FAR_FLOOR_THRESHOLD = 16;
+const float NEAR_FLOOR_THRESHOLD = 0;
+
+bool is_on_wall = false;
+
+// declare functions
+float get_distance_from_wall() {
+  // read from ultrasonic sensor and return distance to the wall      
+}
+
+float get_distance_from_floor(){
+  // read from the ultrasonic sensor in the back of the robot and return distance to the floor
+}
+
+float orient_steering(){
+  // read from joysticks and orient servomotor position
+}
+
+void on_floor(){
+  // controlling orientation of propellers when robot is on the floor 
+}
+
+void floor_to_wall(){
+  // controlling orientation of propellers when robot is transitioning from floor to wall
+}
+
+void on_wall(){
+  // controlling orientation of propellers when robot is on wall
+}
+
+void wall_to_floor(){
+  // controlling orientation of propellers when robot is getting off wall 
+}
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  float distance_to_obstacle = get_distance();
+  if (!is_on_wall){
+    distance_to_obstacle = get_distance_from_wall();
+  }
+
+  else {
+    distance_to_obstacle = get_distance_from_floor();
+  }
+
+  // if value returned by ultrasonic sensor 
+  if (distance == -1.0){
+    return;
+  }
+  
+  // when robot is on the floor
+  if (!is_on_wall){
+    // when robot is far away from wall, robot movement will be controlled by joysticks/propellers
+    if (distance_to_obstacle > FAR_WALL_THRESHOLD) {
+      on_floor();
+      orient_steering();
+    }
+  
+    // when robot is transitioning from floor to wall
+    else if (distance_to_obstacle <= FAR_WALL_THRESHOLD && distance_to_obstacle >= NEAR_WALL_THRESHOLD) {
+      floor_to_wall();
+      is_on_wall = true; 
+    }
+  }
+
+  // when robot is on the wall
+  else {
+    // when robot is far away from floor, robot movement will be controlled by joysticks/propellers
+    if (distance_to_obstacle > FAR_FLOOR_THRESHOLD){
+      on_wall();
+    }
+    
+    // when robot is transitioning from wall to floor
+    else if (distance_to_obstacle <= FAR_FLOOR_THRESHOLD && distance_to_obstacle >= NEAR_FLOOR_THRESHOLD){
+      wall_to_floor();
+      is_on_wall = false;
+    }
+  }
+}
