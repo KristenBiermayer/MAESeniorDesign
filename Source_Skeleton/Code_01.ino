c4e355da1e1aacae5ee500c957eb02d5ce4fce40
Susan Su <susansu@ucla.edu>
2017-05-26 00:42:50 -0700

    Create Code_01.ino

---
+++ Source_Skeleton/Code_01.ino
@@ -0,0 +1,35 @@
+#include <Servo.h>
+#include <SoftwareSerial.h>
+
+const int pot_pin = A0;
+const int joystick_pin = A1;
+const int button_pin = 7;
+
+
+SoftwareSerial xbee(2,3);
+
+void setup() {
+  Serial.begin(9600); 
+  xbee.begin(9600);
+  pinMode(button_pin,INPUT);
+}
+
+void loop() {
+  if (xbee.available()) {
+    String command_input = xbee.readStringUntil('\n');
+    Serial.println(command_input);
+    char id_char = command_input.charAt(0);
+    if (id_char == 'p') {
+      speed_source();
+    }
+    else if (id_char == 'j') {
+      steering_source();
+    }
+    else if (id_char == 's') {
+      button_source_setup();
+    }
+    else if (id_char == 'm') {
+      button_source_mount ();
+    }
+  }
+}
